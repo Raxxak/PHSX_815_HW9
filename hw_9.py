6413cf3d54ade714f438b0e7b8a85ad09a98970f
Raxxak <78438510+Raxxak@users.noreply.github.com>
2021-03-29 22:31:58 -0500

    Add files via upload

--- hw_9.py
+++ hw_9.py
@@ -1,17 +1,34 @@
 # imports packages
 import numpy as np
 from scipy.optimize import minimize
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 
 #Define a function    
 def function(x):
-    return  x+x**2+x**4
+    return  pow(x,2)+4*x
 #It minimizes the  function
 
 minimum_value = minimize(function,0)
     
 print('minimum value= ',minimum_value.x)
+
+#Visualization part
+
+x=[]
+y=[]
+j=0
+for j in range(60):
+    i=j/10-5
+    x.append(i)
+    y.append(function(i))
+
+
+plt.plot(x,y)
+plt.plot([],[],label='minimum= '+str(minimum_value.x))
+plt.legend()
+plt.show()
+    
     
    
